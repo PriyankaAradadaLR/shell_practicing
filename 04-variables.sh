20262f3a5a501613bf5b0006015e05eb36a4075c
Priyanka <aradadadevipriyanka.com>
2026-02-03 15:14:08 +0530

    varables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,8 +3,8 @@
 PERSON1=Lohitha
 PERSON2=Rudra
 
-echo "$PERSON1 : Hi PERSON2 bro  how are you?"
-echo "$PERSON2 : Hi PERSON1 sister i am good what about you"
-echo "$PERSON1 : I am fine . what you are doing PERSON2?"
+echo "$PERSON1 : Hi $PERSON2 bro  how are you?"
+echo "$PERSON2 : Hi $PERSON1 sister i am good what about you"
+echo "$PERSON1 : I am fine . what you are doing $PERSON2?"
 echo "$PERSON2 : Just now i  have taken my lunch"
 
