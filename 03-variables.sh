01951502311e2c9405071c218d7688a144e029be
Priyanka <aradadadevipriyanka.com>
2026-02-03 15:03:58 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "$Lohitha : Hi Rudra bro  how are you?"
+echo "$Rudra : Hi Lohitha sister i am good what about you"
+echo "$Lohith : I am fine . what you are doin?"
+echo "$Rudra : Just now i  have taken my lunch"
+
