7359829b4612516f5c308bc1670dd78dce336eb3
Priyanka <aradadadevipriyanka.com>
2026-02-04 12:09:58 +0530

    conditions

---
+++ 11-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# -gt ->greater than
+# -lt ->less than
+# -eq ->equal
+# -ne ->not equal
+
+NUM=$1
+
+if($NUM -gt 30)
+    echo " given number: $NUM is grester than 30"
+else 
+    echo "given number :$NUM  is less than 30"
+fi
