96f2afaf745a58e5748d1eae1e181699e527511f
Priyanka <aradadadevipriyanka.com>
2026-02-04 11:26:22 +0530

    Time

---
+++ 09-special_variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### all special characters###
+
+echo "all aruguments passed : $@"
+echo "PID of the script is  : $$"
+echo "who is the user       : $USER"
+echo "Home directory of the current user : $HOME"
+echo "Script name           : $0"
+sleep 10
+echo "PID of recently executed background process:$!"
+echo "Present working directory" : $PWD"
+echo "All arguments passed to script :$*"
+echo "Number of Arguments passed to the script : $#"
